af9d7531ab7822dd42c7cbc6dd82bd4c4ff9a2d0
mounir-laity <mounirlaity@gmail.com>
2021-12-05 16:09:26 +0100

    created node and graph classes

--- maze.py
+++ maze.py
@@ -1,3 +1,23 @@
-class maze:
-    def __init__(self) -> None:
-        pass
+from typing import List
+
+
+class Node:
+    def __init__(self, row, column, parent) -> None:
+        self.row = row
+        self.column = column
+        self.parent = parent
+
+
+class Graph:
+    def __init__(self, rows, columns) -> None:
+        self.nodes = List[List[Node]]
+        for row in range(rows):
+            list_nodes = []
+            for column in range(columns):
+                list_nodes.append(Node(row=row, column=column, parent=self))
+            self.nodes.append(list_nodes)
+
+    def get_neighbours(self, current_node: Node):
+        row = current_node.row
+        column = current_node.column
+        list_neighbours = []
