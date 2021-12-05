2b08dd4522ba0f5d73ff42b25dcb0bc1d3da7035
mounir-laity <mounirlaity@gmail.com>
2021-12-05 20:15:50 +0100

    added the get_neighbours method

--- maze.py
+++ maze.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Node:
-    def __init__(self, row, column, parent) -> None:
+    def __init__(self, row: int, column: int, parent) -> None:
         self.row = row
         self.column = column
         self.parent = parent
@@ -10,21 +10,47 @@ class Node:
     def set_parent(self, new_parent):
         self.parent = new_parent
 
+    def __eq__(self, other):
+        return self.row == other.row and self.column == other.column
+
+    def get_position(self):
+        return self.row, self.column
+
 
 class Graph:
     def __init__(self, rows, columns) -> None:
-        self.nodes = List[List[Node]]
+        self.nodes = []
         for row in range(rows):
             list_nodes = []
             for column in range(columns):
                 list_nodes.append(Node(row=row, column=column, parent=None))
             self.nodes.append(list_nodes)
 
-    def get_neighbours(self, current_node: Node):
+    def get_neighbours(self, current_node: Node, allow_diagonal=True):
         row = current_node.row
         column = current_node.column
         list_neighbours = []
-        for i in range(row - 1, row + 1):
-            for j in range(column - 1, column + 1):
-                list_neighbours.append(self.nodes(i, j))
+        if allow_diagonal:
+            for i in range(row - 1, row + 2):
+                for j in range(column - 1, column + 2):
+                    if i != current_node.row or j != current_node.column:
+                        list_neighbours.append(self.nodes[i][j])
+        else:
+            for i in range(row - 1, row + 2):
+                for j in range(column - 1, column + 2):
+                    if (i == row and not j == column) or (
+                        not i == row and j == column
+                    ):  # xor
+                        list_neighbours.append(self.nodes[i][j])
         return list_neighbours
+
+    def get_node(self, row, column):
+        return self.nodes[row][column]
+
+
+if __name__ == "__main__":
+    my_graph = Graph(10, 10)
+    my_list = my_graph.get_neighbours(my_graph.get_node(3, 3), False)
+    for my_node in my_list:
+        print(my_node.get_position())
+    # print(my_graph.get_node(3, 3).get_position())
