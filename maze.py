af86c09b5a75263972e2c64cf228bf9f4a148ead
mounir-laity <mounirlaity@gmail.com>
2021-12-05 19:16:15 +0100

    added graph class

--- maze.py
+++ maze.py
@@ -7,6 +7,9 @@ class Node:
         self.column = column
         self.parent = parent
 
+    def set_parent(self, new_parent):
+        self.parent = new_parent
+
 
 class Graph:
     def __init__(self, rows, columns) -> None:
@@ -14,10 +17,14 @@ class Graph:
         for row in range(rows):
             list_nodes = []
             for column in range(columns):
-                list_nodes.append(Node(row=row, column=column, parent=self))
+                list_nodes.append(Node(row=row, column=column, parent=None))
             self.nodes.append(list_nodes)
 
     def get_neighbours(self, current_node: Node):
         row = current_node.row
         column = current_node.column
         list_neighbours = []
+        for i in range(row - 1, row + 1):
+            for j in range(column - 1, column + 1):
+                list_neighbours.append(self.nodes(i, j))
+        return list_neighbours
