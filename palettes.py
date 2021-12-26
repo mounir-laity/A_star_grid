b77c3c6a1ef700878f4e3943d6cb910ec0cfbb4b
mounir-laity <mounirlaity@gmail.com>
2021-12-26 22:12:45 +0100

    added documentation for the palettes enum

--- palettes.py
+++ palettes.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
-# pattern : [fg_color, bg_color, wall_color, start_color, end_color, path_color, explored_color, palette n°]
+# pattern : [fg_color, bg_color, wall_color, start_color, end_color, path_color, explored_color, palette number]
 class Palettes(Enum):
+    """This represents the possible color palettes possibilities for the A star application."""
+
     DEFAULT = [
         "#D2D4C8",
         "#8B2635",
@@ -12,7 +14,6 @@ class Palettes(Enum):
         "pink",
         0,
     ]
-    # https://coolors.co/2b2d42-8d99ae-edf2f4-ef233c-d90429
     STEEL = [
         "#EDF2F4",
         "#8D99AE",
@@ -23,7 +24,6 @@ class Palettes(Enum):
         "#F68D99",
         1,
     ]
-    # https://coolors.co/edf5e1-5cdb95-05386b-e0e2db-def3ce-4987ab-379683
     ENERGY = [
         "#EDF5E1",
         "#5CDB95",
